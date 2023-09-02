d6e9230dd0282925b91d9226b1116fb854c6273a
Ganesh Hake <121447447+ganesh454770@users.noreply.github.com>
2023-09-02 15:32:21 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ezobooks';
+
+  public image: any = []
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getData() {
+    const Url = 'https://db.ezobooks.in/kappa/image/task'
+    this.http.get(Url).subscribe((res) => {
+      this.image = res;
+
+      console.log(this.image)
+    })
+  }
+  ngOnInit(): void {
+
+    //const url : string = 'https://db.ezobooks.in/kappa/image/task'
+    //this.http.get(url).subscribe((res) => {
+     // this.items = res;
+    //})
+
+    this.getData()
+  }
 }
